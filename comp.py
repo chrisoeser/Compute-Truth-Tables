c707c0127e44045c0180bd4cfc8951fdca31a167
chris <c.f.oeser2@newcastle.ac.uk>
2023-01-09 12:32:55 +0000

    made the truth tables

--- comp.py
+++ comp.py
@@ -1,15 +1,32 @@
-
-'''print('Boolean expression 1')
-b1 = input()
-print('Boolean expression 2')
-b2 = input()
-'''
+import itertools
+import re
 
 
-def table():
-    for i in range(5):
-        for j in range(3):
-            print('___', end='')
-        print()
+def truth_table(expression):
+    # Define the variables in the expression
+    variables = sorted(set(re.findall(r'\b[A-Za-z]\b', expression)))
 
-table()
+    # Determine the width of the longest variable
+    width = max(len(v) for v in variables)
+
+    # Print the header row
+    print('+' + '+'.join(['-' * (width + 2) for v in variables]) + '+--------+')
+    print('| ' + ' | '.join(v.center(width) for v in variables) + ' | Result |')
+    print('+' + '+'.join(['-' * (width + 2) for v in variables]) + '+--------+')
+
+    # Generate all possible combinations of values for the variables
+    for values in itertools.product([True, False], repeat=len(variables)):
+        # Create a dictionary mapping variables to values
+        assignment = dict(zip(variables, values))
+
+        # Evaluate the expression under this assignment of values
+        result = eval(expression, assignment)
+
+        # Print the values of the variables and the result of the expression
+        print('| ' + ' | '.join(str(int(v)).rjust(width) for v in values) + ' | ' + str(result).ljust(7) + '|')
+
+    # Print the footer row
+    print('+' + '+'.join(['-' * (width + 2) for v in variables]) + '+--------+')
+
+
+truth_table("(A and B) or (C and D)")
